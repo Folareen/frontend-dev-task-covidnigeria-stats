31e230b624960a3a8535e9889983f8fc13bde057
Folareen <sakawahab03@gmail.com>
2022-08-22 15:07:52 +0100

    added loading state

--- src/App.js
+++ src/App.js
@@ -1,47 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatesStat } from "./components/StateStat";
 import TotalStatCard from "./components/TotalStatCard";
 import "./styles/css/index.css";
 import displayDateAndTime from "./utils/displayDateAndTime";
+import { TailSpin } from "react-loader-spinner";
 
 const App = () => {
+  const [statsData, setStatsData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async function () {
+      const response = await fetch("https://covidnigeria.herokuapp.com/api");
+      const data = await response.json();
+      console.log(data);
+      console.log("done!");
+      setStatsData(data);
+      setLoading(false);
+      console.log(statsData);
+    })();
+  }, []);
+
   return (
     <main>
       <header>
-        <h1>Covid-19 Statistics in Nigeria</h1>
+        <h1>
+          <span style={{ color: "darkred" }}>Covid-19</span> Statistics in
+          <span style={{ color: "darkgreen" }}> Nigeria</span>
+        </h1>
         <p>{displayDateAndTime()}</p>
       </header>
 
-      <div>
-        <TotalStatCard statBg={"red"} statTitle={"death"} statValue={30000} />
-      </div>
+      {loading ? (
+        <div className="loading">
+          <TailSpin
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            visible={true}
+          />
+          Fetching Data...
+        </div>
+      ) : (
+        <>
+          <div>
+            <TotalStatCard
+              statBg={"red"}
+              statTitle={"death"}
+              statValue={30000}
+            />
+          </div>
 
-      <div className="stat--table-container">
-        <h2>Confirmed Cases for each state:</h2>
-        <table>
-          <tr>
-            <th>States</th>
-            <th>Confirmed Cases</th>
-            <th>Cases on Admission</th>
-            <th>Discharged Cases</th>
-            <th>Death Cases</th>
-          </tr>
-          <StatesStat
-            stateName={"lagos"}
-            confirmedCases={2344}
-            dishargedCases={8944}
-            admittedCases={9494}
-            deathStat={94}
-          />
-          <StatesStat
-            stateName={"lagos"}
-            confirmedCases={2344}
-            dishargedCases={8944}
-            admittedCases={9494}
-            deathStat={94}
-          />
-        </table>
-      </div>
+          <div className="stat--table-container">
+            <h2>Confirmed Cases for each state:</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>States</th>
+                  <th>Confirmed Cases</th>
+                  <th>Cases on Admission</th>
+                  <th>Discharged Cases</th>
+                  <th>Death Cases</th>
+                </tr>
+              </thead>
+              <tbody>
+                <StatesStat
+                  stateName={"lagos"}
+                  confirmedCases={2344}
+                  dishargedCases={8944}
+                  admittedCases={9494}
+                  deathStat={94}
+                />
+                <StatesStat
+                  stateName={"lagos"}
+                  confirmedCases={2344}
+                  dishargedCases={8944}
+                  admittedCases={9494}
+                  deathStat={94}
+                />
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
     </main>
   );
 };
